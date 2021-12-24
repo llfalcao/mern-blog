3dbcb877dd80f06bb11bc1264a6fba397d0be1b7
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-24 20:44:56 -0300

    Refactor route handling

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
+import { router as routeHandler } from './routes';
 
 const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({
+    message:
+      'Welcome to the blog API! Refer to https://github.com/llfalcao/blog for the documentation.',
+  });
+});
+
+app.use('/api/v1', routeHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,12 @@
+import { Router } from 'express';
 import users from './users';
 import posts from './posts';
 import comments from './comments';
 
-export { users, posts, comments };
+const router = Router();
+
+router.use('/users', users);
+router.use('/posts', posts);
+router.use('/comments', comments);
+
+export { router };
