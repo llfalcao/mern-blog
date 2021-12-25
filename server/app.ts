1a67c48bb918935541495d18c952e9b4bd1f0141
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-24 22:47:39 -0300

    Handle database connection

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,15 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { router as routeHandler } from './routes';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
+
+const mongoDB = process.env.MONGODB || '';
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 app.get('/api', (req, res) => {
   res.json({
